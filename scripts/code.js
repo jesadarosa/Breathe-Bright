a951d3ffeb5f1856f9d7cceab8a9a9fa9cb41467
jesadarosa <131052987+jesadarosa@users.noreply.github.com>
2024-04-21 23:04:11 -0400

    Update code.js

--- scripts/code.js
+++ scripts/code.js
@@ -1,1 +1,14 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+            var modal = document.getElementById("videoModal");
+            var video = document.getElementById("introVideo");
+
+            // Show the modal when the page loads
+            modal.style.display = "block";
+
+            // Pause the video when the modal is closed
+            modal.addEventListener("click", function () {
+                modal.style.display = "none";
+                video.pause();
+            });
+        });
